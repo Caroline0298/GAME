7eaaa0ee882f3df06570f7d23a8694acd90a22eb
Caroline Nascimento <42359565+Caroline0298@users.noreply.github.com>
2019-11-12 10:13:14 -0300

    Game de nave

---
+++ GameControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public int qntInimigo, maxInimigos;
+    public GameObject inimigo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Instantiate(inimigo, new Vector3(Random.Range(-8.3f, 8.3f), 
+                                         transform.position.y, 
+                                         transform.position.z), 
+                                         Quaternion.identity);
+        qntInimigo++;
+        if(qntInimigo < maxInimigos)
+        {
+            StartCoroutine(Delay());
+        }
+    }
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left")) {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet, this.transform.position, Quaternion.identity);
+        }
+    }
+}
